c7415febbc5698ef5569dbe496c981cb0acf77f1
Grahame Bowland <grahame@oreamnos.com.au>
2019-06-14 03:21:56 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 extern crate clap;
 extern crate dividebatur;
+extern crate rayon;
 extern crate serde;
 extern crate serde_derive;
 extern crate toml;
 
 use clap::{App, Arg};
+use rayon::prelude::*;
 use serde_derive::Deserialize;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fs::File;
 use std::io::Read;
+use dividebatur::engine::*;
 
 #[derive(Debug, Deserialize)]
 struct Candidates {
@@ -68,6 +71,10 @@ fn read_config(input_file: &str) -> Result<Config, String> {
 #[derive(Debug)]
 struct CountTask {
     format: String,
+    state: String,
+    candidates: String,
+    preferences: String,
+    vacancies: usize,
     slug: String,
 }
 
@@ -90,14 +97,65 @@ fn get_counts(input_files: Vec<&str>) -> Work {
         let mut new = config
             .count
             .iter()
-            .map(|(slug, x)| CountTask {
+            .map(|(slug, count)| {
+                let dataset = match config.dataset.get(&count.dataset) {
+                    Some(d) => d,
+                    None => {
+                        panic!();
+                    }
+                };
+                CountTask {
+                    state: slug.clone(),
                 slug: slug.clone(),
+                candidates: config.candidates.all.clone(),
+                preferences: dataset.preferences.clone(),
                 format: config.format.clone(),
-            })
+                vacancies: count.vacancies,
+            }})
             .collect();
         work.counts.append(&mut new);
     }
     work
+}
+
+fn run_task(task: &CountTask) -> Result<bool, String> {
+    let candidates = match dividebatur::aec::data::candidates::load(&task.candidates, &task.state) {
+        Ok(rows) => rows,
+        Err(error) => {
+            return Err(format!("Couldn't read candidates file: {:?}", error));
+        }
+    };
+    let cd = dividebatur::senate2015::load_candidate_data(candidates);
+
+    let prefpath = &task.preferences;
+    let ballot_states =
+        dividebatur::aec::data::formalpreferences::read_file(prefpath, &cd.tickets, cd.count);
+    println!("len {}", cd.tickets.len());
+
+    println!(
+        "{} unique bundle states at commencement of count.",
+        ballot_states.len()
+    );
+
+    let mut automation = VecDeque::new();
+    automation.push_back(0);
+    let mut engine = CountEngine::new(task.vacancies as u32, cd, ballot_states, automation);
+    while {
+        let outcome = engine.count();
+        match outcome {
+            CountOutcome::CountComplete(nrounds, state) => {
+                // engine.print_debug();
+                println!("{:?}", state);
+                println!("Election complete after {} rounds of counting.", nrounds);
+                false
+            }
+            CountOutcome::CountContinues(_, _) => {
+                // engine.print_debug();
+                true
+            }
+        }
+    } {}
+    Ok(true)
 }
 
 fn main() {
@@ -113,6 +171,13 @@ fn main() {
         )
         .get_matches();
 
-    let config = get_counts(matches.values_of("INPUT").unwrap().collect());
-    println!("config: {:?}", config);
+    let work = get_counts(matches.values_of("INPUT").unwrap().collect());
+    let _x: Vec<_> = work
+        .counts
+        .par_iter()
+        .map(|x| {
+            let r = run_task(x);
+            println!("{:?} -> {:?}", x, r);
+        })
+        .collect();
 }
--- src/senate2015.rs
+++ src/senate2015.rs
@@ -1,8 +1,5 @@
 use aec;
 use defs::*;
-use engine::*;
-use rayon::prelude::*;
-use std::collections::VecDeque;
 
 pub fn load_candidate_data(
     candidates: Vec<aec::data::candidates::AECAllCandidateRow>,
@@ -38,49 +35,4 @@ pub fn load_candidate_data(
         parties,
         tickets,
     }
-}
-
-fn run_state(state: &str, vacancies: usize) -> bool {
-    let candidates = match aec::data::candidates::load(
-        "dividebatur-aec/fed2016/common/aec-senate-candidateinformation-20499.csv",
-        state,
-    ) {
-        Ok(rows) => rows,
-        Err(error) => {
-            panic!("Couldn't read candidates file: {:?}", error);
-        }
-    };
-    let cd = load_candidate_data(candidates);
-    let prefpath = format!(
-        "dividebatur-aec/fed2016/{}/data/aec-senate-formalpreferences-20499-{}.csv.gz",
-        state.to_ascii_lowercase(),
-        state.to_ascii_uppercase()
-    );
-    let ballot_states = aec::data::formalpreferences::read_file(&prefpath, &cd.tickets, cd.count);
-    println!("len {}", cd.tickets.len());
-
-    println!(
-        "{} unique bundle states at commencement of count.",
-        ballot_states.len()
-    );
-
-    let mut automation = VecDeque::new();
-    automation.push_back(0);
-    let mut engine = CountEngine::new(vacancies as u32, cd, ballot_states, automation);
-    while {
-        let outcome = engine.count();
-        match outcome {
-            CountOutcome::CountComplete(nrounds, state) => {
-                // engine.print_debug();
-                println!("{:?}", state);
-                println!("Election complete after {} rounds of counting.", nrounds);
-                false
-            }
-            CountOutcome::CountContinues(_, _) => {
-                // engine.print_debug();
-                true
-            }
-        }
-    } {}
-    true
 }
